bb2554467aa0ffce7a33dccb49d84d548c905ba1
xinrui98 <gao.xinrui98@gmail.com>
2020-10-18 17:31:44 +0800

    Initial commit

---
+++ lib/screens/location_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LocationScreen extends StatefulWidget {
+  @override
+  _LocationScreenState createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
